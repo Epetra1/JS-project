96b388ed94bdcd14d42f7ca2e6f1438fa8ee769e
Epetra1 <dhunganac5@gmail.com>
2024-07-30 18:20:43 +1000

    export paymentSummery function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,6 @@
 import {cartItemRender} from './checkout/orderSummery.js'
+import {paymentSummery} from './checkout/payment.js'
+
 
 cartItemRender();
+paymentSummery();
---
+++ scripts/checkout/payment.js
@@ -0,0 +1,3 @@
+export function paymentSummery(){
+    console.log('hello')
+}
